79ac43b091a851d2696e62877b22086de369dc58
itsyourOOP <absolutelysplashing@gmail.com>
2017-10-15 18:16:19 -0400

    updated aliases.zsh

--- zsh_files/aliases.zsh
+++ zsh_files/aliases.zsh
@@ -1,3 +1,66 @@
+
+# NeoVim
+#alias vim="nvim"
+#alias vi="nvim"
+#alias v="nvim"
+
+# Tmux
+alias tmux="tmux -2"
+
+# Vagrant
+alias vu='vagrant up'
+alias vs='vagrant ssh'
+alias vh='vagrant halt'
+alias vss='vagrant status'
+
+## Git
+alias gst="git status -s"
+alias ga="git add"
+alias gg="git log --oneline --graph --decorate --color"
+unalias gd
+alias gd="git diff"
+
+## Zshmarks
+alias g="jump"
+alias s="bookmark"
+alias d="deletemark"
+alias p="showmarks"
+alias l="showmarks"
+
+# Find files
+f() {
+    ag --smart-case --hidden -g $1
+}
+
+# Remove orphans packages
+orphans() {
+    sudo pacman -Rns $(pacman -Qtdq)
+}
+
+# Remaining todos in the current folder
+todos() {
+    autoload colors; colors;
+    echo "You have: ($fg_bold[red]`ag -i @todo . | wc -l`$fg[white]) todos left."
+}
+
+# bandwidth
+bt() {
+    wget http://cachefly.cachefly.net/100mb.test -O /dev/null
+}
+
+# from old
+alias yget="yaourt -S"
+alias up='cd ..'
+alias weather="curl wttr.in/Pittsburgh"
+alias moonPhase="curl wttr.in/Moon"
+alias weatherAndMoonPhase="weather && moonPhase"
+alias poweroff="sudo shutdown -h now"
+alias mpsyt="sudo mpsyt"
+alias orochi="sudo orochi"
+alias whenPacmanActsUp="sudo rm /var/lib/pacman/db.lck"
+alias pip2="sudo pip"
+
+#from new
 alias fireplace='~/sources/fireplace/fireplace'
 alias set_bg='~/DotsMine/set_bg'
 alias edit_alias='nano ~/aliases.zsh'
@@ -8,3 +71,4 @@ alias kek='cd && clear'
 alias su_nano='sudo nano'
 alias rtv='rtv --enable-media'
 alias git='hub'
+alias edit_zshrc='nano ~/.zshrc'
